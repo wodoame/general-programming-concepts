79d9405718205eb5f9484919e2b41842ccc36ccc
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-02-16 09:06:04 +0000

    Create Map_lookup_time_complexity.js

---
+++ Map_lookup_time_complexity.js
@@ -0,0 +1,7 @@
+/*
+The time complexity of obtaining an item from a Map or HashMap is O(1) on average. 
+This means that the time it takes to access an item in a Map or HashMap does not increase with the size of the Map or HashMap.
+In the worst case, however, the time complexity of obtaining an item from a Map or HashMap is O(n), where n is the number of elements in the Map or HashMap.
+This can occur when there are hash collisions or when the hash function used by the Map or HashMap is poorly designed, causing a large number of collisions. 
+However, these situations are rare in practice, and the amortized time complexity for obtaining an item from a Map or HashMap is still O(1).
+*/
