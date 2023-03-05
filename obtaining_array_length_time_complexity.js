80970e99bd2e4187b4df23114dbfbd49a69cb8de
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-03-05 07:44:15 +0000

    Create obtaining_array_length_time_complexity.js

---
+++ obtaining_array_length_time_complexity.js
@@ -0,0 +1,6 @@
+// The time complexity for obtaining the length of an array is O(1), 
+// which means it takes a constant amount of time, regardless of the size of the array.
+
+// This is because the length of an array is stored as a property of the array object, 
+// so accessing it does not require iterating through the entire array. Therefore, 
+// the time required to obtain the length of an array is constant and does not depend on the size of the array.
