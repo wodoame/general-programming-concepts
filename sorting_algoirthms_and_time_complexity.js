22f45ebb04989cd97087f91485b1f3529b8a3aa4
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-03-06 03:11:43 -0800

    Create sorting_algoirthms_and_time_complexity.js

---
+++ sorting_algoirthms_and_time_complexity.js
@@ -0,0 +1,117 @@
+/**
+ * In computer science, sorting algorithms are used to rearrange a list of elements into a specific order. 
+ * There are many different algorithms for sorting, each with its own strengths and weaknesses.
+ * One important factor to consider when choosing a sorting algorithm is its time complexity, 
+ * which determines how long it takes to sort a list of a certain size.
+ */
+
+/**
+ * Bubble Sort:
+ * A simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order.
+ * Time Complexity:
+ * Worst Case - O(n^2)
+ * Average Case - O(n^2)
+ * Best Case - O(n)
+ */
+function bubbleSort(arr) {
+  // implementation
+}
+
+/**
+ * Selection Sort:
+ * A sorting algorithm that selects the smallest element from an unsorted list in each iteration and places that element at the beginning of the list.
+ * Time Complexity:
+ * Worst Case - O(n^2)
+ * Average Case - O(n^2)
+ * Best Case - O(n^2)
+ */
+function selectionSort(arr) {
+  // implementation
+}
+
+/**
+ * Insertion Sort:
+ * A simple sorting algorithm that builds the final sorted array one item at a time. 
+ * It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.
+ * Time Complexity:
+ * Worst Case - O(n^2)
+ * Average Case - O(n^2)
+ * Best Case - O(n)
+ */
+function insertionSort(arr) {
+  // implementation
+}
+
+/**
+ * Merge Sort:
+ * A divide-and-conquer algorithm that recursively divides the input array into two halves, sorts each half, and then merges the sorted halves to produce a sorted output.
+ * Time Complexity:
+ * Worst Case - O(n log n)
+ * Average Case - O(n log n)
+ * Best Case - O(n log n)
+ */
+function mergeSort(arr) {
+  // implementation
+}
+
+/**
+ * Quick Sort:
+ * A divide-and-conquer algorithm that selects a "pivot" element from the array and partitions the other elements into two sub-arrays, 
+ * according to whether they are less than or greater than the pivot. The sub-arrays are then sorted recursively.
+ * Time Complexity:
+ * Worst Case - O(n^2)
+ * Average Case - O(n log n)
+ * Best Case - O(n log n)
+ */
+function quickSort(arr) {
+  // implementation
+}
+
+/**
+ * Heap Sort:
+ * A comparison-based sorting algorithm that uses a binary heap data structure to sort elements.
+ * Time Complexity:
+ * Worst Case - O(n log n)
+ * Average Case - O(n log n)
+ * Best Case - O(n log n)
+ */
+function heapSort(arr) {
+  // implementation
+}
+
+/**
+ * Counting Sort:
+ * A non-comparison based sorting algorithm that works by counting the number of objects having distinct key values and sorting them according to the keys.
+ * Time Complexity:
+ * Worst Case - O(n + k)
+ * Average Case - O(n + k)
+ * Best Case - O(n + k)
+ */
+function countingSort(arr, max) {
+  // implementation
+}
+
+/**
+ * Radix Sort:
+ * A non-comparison based sorting algorithm that sorts data with integer keys by grouping the keys by individual digits that share the same significant position and value.
+ * Time Complexity:
+ * Worst Case - O(nk)
+ * Average Case - O(nk)
+ * Best Case - O(nk)
+ */
+function radixSort(arr) {
+  // implementation
+}
+
+/**
+ * Bucket Sort:
+ * A non-comparison based sorting algorithm that works by distributing the elements of an array into a number of buckets. 
+ * Each bucket is then sorted using another algorithm or by recursively applying the bucket sorting algorithm.
+ * Time Complexity:
+ * Worst Case - O(n^2)
+ * Average Case - O(n + k)
+ * Best Case - O(n)
+ */
+function bucketSort(arr, numBuckets) {
+  // implementation
+}
